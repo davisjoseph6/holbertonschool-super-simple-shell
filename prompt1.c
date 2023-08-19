24ee88229e4dd6723804a9a61f04ce73ea02895b
abdel <holbertonc21@gmail.com>
2023-08-19 06:03:04 -0700

    prompt1.c

--- prompt1.c
+++ prompt1.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 
 int main(void) {
-    char *buffer = NULL;  // Initialize the buffer pointer
-    size_t len = 0;       // Initialize the buffer size
+    char *buffer = NULL;
+    size_t len = 0;
+    ssize_t read;
 
     while (1) {
         printf("$ ");
-        ssize_t read = getline(&buffer, &len, stdin);  // Use ssize_t for error handling
+        read = getline(&buffer, &len, stdin);
         if (read == -1) {
             if (feof(stdin)) {
-                // End of file (Ctrl+D) encountered, break out of the loop
                 printf("\n");
                 break;
             } else {
-                // Error occurred while reading input, handle it
                 perror("getline");
-                free(buffer);  // Free allocated memory before exiting
+                free(buffer);
                 return 1;
             }
         }
@@ -24,9 +23,7 @@ int main(void) {
         printf("%s", buffer);
     }
 
-    // Free allocated memory before exiting
     free(buffer);
-
     return 0;
 }
 
