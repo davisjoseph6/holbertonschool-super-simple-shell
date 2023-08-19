16e49e62b2278ee1ea1f38bc4d50ada37b2aef07
abdel <holbertonc21@gmail.com>
2023-08-19 05:23:23 -0700

    av

--- av.c
+++ av.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 
 int main(int ac, char **av)
 {
-	int i = 0;
+    int i = 0;
 
-	while (av[i])
-	{
-		printf("%s ", av[i]);
-		i++;
-	}
-	printf("\n");
-	return (0);
+    /* Using the ac parameter to suppress the unused parameter warning */
+    printf("Number of arguments: %d\n", ac);
+
+    while (av[i])
+    {
+        printf("%s ", av[i]);
+        i++;
+    }
+    printf("\n");
+    return 0;
 }
+
